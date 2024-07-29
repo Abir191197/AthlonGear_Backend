c494c4614a3ae34a2425a35bb68bf7b5efd57fcf
M. Abir Alam <malam19197@bscse.uiu.ac.bd>
2024-07-30 02:12:54 +0600

    folder ignore

---
+++ src/app/module/Products/products.model.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+import { TProduct } from "./products.interface";
+
+const ProductSchema = new Schema<TProduct>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const ProductsModel = mongoose.model<TProduct>("Products", ProductSchema, "Products");
+
+export default ProductsModel;
