4765fc537af0faf5b9bded47fdb250a09b219f1c
M. Abir Alam <malam19197@bscse.uiu.ac.bd>
2024-07-30 02:07:22 +0600

    git ignore

--- src/app/module/Orders/orders.interface.ts
+++
@@ -1,8 +0,0 @@
-import { ObjectId } from "mongoose";
-
-export interface TOrders {
-  productId: ObjectId[];
-  status: "Processing" | "On the Way" | "Delivered";
-  totalAmount: number;
-  OrderBy: ObjectId[];
-}
